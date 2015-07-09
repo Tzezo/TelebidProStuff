cb25998b9369499c0a033219815eec2a1eaf7567
Tzezo <tzvetomir.krastev@gmail.com>
2015-07-10 00:07:31 +0300

    commit

--- DBTests/scriptFork.pl
+++ DBTests/scriptFork.pl
@@ -6,20 +6,57 @@ use Data::Dumper;
 use Time::HiRes qw(usleep);
 use Parallel::ForkManager;
 
+#use Math::Random qw(:all);
+=pod
+my $numb_hist = {};
 
+my $end_time = time + 30;
+my $i = 0;
+while(1)
+{
+    my $a = int(rand(10)) + 1;
+    
+    if(defined $$numb_hist{$a})
+    {   
+        $$numb_hist{$a}++;
+    }
+    else
+    {
+        $$numb_hist{$a} = 1;
+    }
 
+    $i++;
+    
+    my $time = time;
+    if($time > $end_time)
+    {
+        last;
+    }
+    
+}
+
+while(my($key, $val) = each(%$numb_hist))
+{
+    print "$key -> $val\n";
+}
+
+print "Total $i\n";
+
+exit;
+=cut
 
 sub Fork()
 {
     my $pm = new Parallel::ForkManager(100);
-    
-    my $duration = 60; #seconds
 
-    #my $isolation_level = ['SERIALIZABLE','REPEATABLE READ'];
-    my $isolation_level = ['READ COMMITTED'];
-    my $fork_count = [100];
+    my $duration = 15; #seconds
+
+    my $isolation_level = ['SERIALIZABLE','REPEATABLE READ'];
+    #my $isolation_level = ['REPEATABLE READ'];
+    #my $isolation_level = ['READ COMMITTED'];
+    my $fork_count = [10];
     #my $conc_num = [1000];
-    my $sleep = [5];
+    my $sleep = [50000];
     #my $sleep = [1000];
 
     for(my $i = 0; $i < @$isolation_level; $i++)
@@ -40,16 +77,16 @@ sub Fork()
                         $transactions_count += $$data{transactions_count};
 
                     });
-                
+
                 for(my $f = 0; $f < $$fork_count[$k]; $f++)
                 {
                     $pm->start and next;
-                    my $data = Transaction($$fork_count[$k], $duration, $$isolation_level[$i], $$sleep[$s]);        
+                    my $data = Transaction($$fork_count[$k], $duration, $$isolation_level[$i], $$sleep[$s]);
                     $pm->finish(0, $data);
                 }
 
                 $pm->wait_all_children;
-                
+
                 ToCSV($$isolation_level[$i], $$fork_count[$k], $transactions_count, $fail_count, $retry_count, $$sleep[$s], $duration);
             }
         }
@@ -59,9 +96,9 @@ sub Fork()
 sub ToCSV($$$$$)
 {
     my($isolation_level, $fork_count, $transactions_num, $fails, $retries, $sleep, $duration) = @_;
-    
+
     print "$isolation_level, $fork_count, $transactions_num, $fails, $retries, $sleep, $duration\n";
-    
+
     my $fail_rate = $fails/( $transactions_num/100 );
 
     my $csv = "$isolation_level, $fork_count, $transactions_num, $fails, $retries, $sleep, $duration, $fail_rate\n";
@@ -78,36 +115,47 @@ sub ToCSV($$$$$)
 sub Transaction($$$)
 {
     my($fork_count, $duration, $isolation_level, $sleep) = @_;
-    
+
     my $retry_count = 0;
     my $fail_count = 0;
     my $transactions_count = 0;
-    
+
     my $end_time = time + $duration;
 
     my $dbh = DBI->connect('dbi:Pg:dbname=transactionstest;host=localhost','postgres','parola123',{AutoCommit=>0,RaiseError=>1,PrintError=>0});
-        
+
     my $query = "";
     my $query2 = "";
+    my ($a, $b, $a2, $b2);
+
     while(1)
     {
-        
+
         try
         {
             my $sth = $dbh->prepare("SET TRANSACTION ISOLATION LEVEL $isolation_level") or die;
             $sth->execute();
-            
-            #$sth = $dbh->prepare("select sum(a) from bar;");
-            #$sth->execute();
+
             #my $row = $sth->fetchrow_hashref();
+                
 
             if(!$query && !$query2)
             {
+                srand();    
+                
                 my $time = time % 10000;
-                my $a = int(rand($fork_count)) + 1;
-                my $a2 = int(rand($fork_count)) + 1;
-                my $b = "b='$time'";
-                my $b2 = "b='$time'";
+                
+                my $randnumb = $fork_count;
+                
+                #$randnumb = 999999;
+
+                $a = int(rand($randnumb)) + 1;
+                $a2 = int(rand($randnumb)) + 1;
+            
+                #$a = int(rand(100)) + 1;
+                
+                $b = "b='$time'";
+                $b2 = "b='$time'";
 
                 if($isolation_level eq "READ COMMITTED")
                 {
@@ -116,30 +164,37 @@ sub Transaction($$$)
                     $b2 = "b=b+1";
                     $a2 = 1;
                 }
-                    
-                $query = "update bar set $b where a='$a'";
-                $query2 = "update bar set $b2 where a='$a2'";
+
+                $query = "update foo set $b where a='$a'";
+                #$query2 = "update foo set $b2 where a='$a2'";
             }
             else
             {
                 $retry_count++;
             }
-            
-            $transactions_count++;
 
-            print "$query\n\n";
+            $transactions_count++;
+            
+            #$sth = $dbh->prepare("select * from foo where a='$a';");
+            #$sth->execute();
+
             $sth = $dbh->prepare($query);
-            $sth->execute(); 
-            
-            usleep($sleep);
-            $sth = $dbh->prepare($query2);
             $sth->execute();
-            
-            #$sth = $dbh->prepare("select sum(a) from bar;");
-            usleep($sleep);
+
+            #usleep($sleep);
+            #$sth = $dbh->prepare($query2);
+            #$sth->execute();
+
+            $sth = $dbh->prepare("select * from foo where a='$a';");
+            #usleep($sleep);
             $dbh->commit();
+    
+            
+            print "QUERRY: $query $$\n\n";
+
             $query = "";
-            $query2 = "";
+
+            #$query2 = "";
             #last REDIRECT_LOOP;
             #print "bar\n";
         }
@@ -148,10 +203,17 @@ sub Transaction($$$)
             my ($err) = @_;
             $dbh->rollback();
             print $err;
+            print "$query $$\n\n";
             
+            my $rnd = int(rand(4));
+
+            #usleep(200000 * $rnd);
+    
+            #$query = "";
+
             $fail_count++;
         };
-        
+    
         my $time = time;
         if($time > $end_time)
         {
@@ -160,7 +222,7 @@ sub Transaction($$$)
 
    }
    $dbh->disconnect;
-   
+
    return {fail_count => $fail_count, transactions_count => $transactions_count, retry_count => $retry_count};
 }
 
@@ -187,4 +249,3 @@ my $sleep = [0, 50, 500];
         }
     }
 =cut
-
